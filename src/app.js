0b6fe53f963be79132016340a770d4451e762bf9
Jamil Safdar <jamil.safdar@ao.com>
2021-11-12 11:08:53 +0000

    inc update and delete Movies func

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 const yargs = require("yargs");
-const { addMovie, listMovies } = require("./movie/movie.method");
+const { addMovie, listMovies, updateMovie, deleteMovie } = require("./movie/movie.method");
 const command = process.argv[2];
 
 const app = async () => {
-    if (command === "add") {
-      addMovie({ title: yargs.argv.title, actor: yargs.argv.actor });
-    } else if (command === "list") {
-        listMovies(); 
-    } else {
-        console.log("Incorrect command")
-    }
+  if (command === "add") {
+    addMovie({ title: yargs.argv.title, actor: yargs.argv.actor });
+  } else if (command === "list") {
+    listMovies();
+  } else if (command === "update") {
+    const searchMovie = { title: yargs.argv.title };
+    const updateValues = { actor: yargs.argv.actor };
+    await updateMovie(searchMovie, updateValues);
+  } else if (command === "delete") {
+    const findMovie = {
+      title: yargs.argv.title,
+    };
+    await deleteMovie(findMovie);
+  } else {
+    console.log("Incorrect command");
+  }
 };
 app();
--- src/movie/movie.method.js
+++ src/movie/movie.method.js
@@ -1,24 +1,44 @@
-const Movie = require('./movie.model');
+const Movie = require("./movie.model");
 
 exports.addMovie = async (movieObj) => {
-    
-    try {
-      await Movie.sync();
-      await Movie.create(movieObj);
+  try {
+    await Movie.sync();
+    await Movie.create(movieObj);
 
-      console.log("Movie successfully added to db");
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    console.log("Movie successfully added to db");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-  exports.listMovies = async () => {
-    try {
-        await Movie.sync();
-        const list = await Movie.findAll();
-          console.log(list);
+exports.listMovies = async () => {
+  try {
+    await Movie.sync();
+    const list = await Movie.findAll({
+      raw: true,
+      attributes: ["id", "title", "actor"],
+    });
+    console.log(list);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.updateMovie = async ( searchMovie, updateValues) => {
+  try {
+    await Movie.update(updateValues, { where: searchMovie });
+    console.log(`${searchMovie.title} has been updated`);
  
-    } catch (error) {
-        console.log(error);
-    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.deleteMovie = async (searchMovie) => {
+  try {
+      await Movie.destroy({ where: searchMovie });
+      console.log(`${searchMovie.title} has been deleted`);
+  } catch (error) {
+      console.log(error);
+  }
 };
